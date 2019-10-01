12565b2252b9902b96012da54a5b34a0c794229f
Renuka Fernando <renukapiyumal@gmail.com>
2019-10-01 10:36:32 +0530

    configure spring boot application with hello rest api

---
+++ src/main/java/com/mitrai/sample/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.mitrai.sample.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/{name}")
+    public String hello(@PathVariable String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/com/mitrai/sample/SampleApplicationTest.java
@@ -0,0 +1,20 @@
+package com.mitrai.sample;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple SampleApplication.
+ */
+public class SampleApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
